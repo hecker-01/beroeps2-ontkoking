e78b29420b5311cb9e2fa4816a8f2ef04d8e10f9
sjoerdsteen <101756@glr.nl>
2025-10-30 12:15:43 +0100

    light/dark

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+const themeSwitch = document.getElementById('theme-switch');
+const body = document.body;
+
+// Controleer opgeslagen voorkeur
+if (localStorage.getItem('theme') === 'light') {
+    body.classList.add('light-mode');
+    themeSwitch.checked = true;
+}
+
+themeSwitch.addEventListener('change', () => {
+    if (themeSwitch.checked) {
+        body.classList.add('light-mode');
+        localStorage.setItem('theme', 'light');
+    } else {
+        body.classList.remove('light-mode');
+        localStorage.setItem('theme', 'dark');
+    }
+});
