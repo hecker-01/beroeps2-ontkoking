76d02c003673ada4e13ec747ad292e9aa4f22f4a
sjoerdsteen <101756@glr.nl>
2025-11-06 10:03:15 +0100

    updated light/dark

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,18 @@
 const themeSwitch = document.getElementById('theme-switch');
 const body = document.body;
 
-// Controleer opgeslagen voorkeur
+// Load stored theme
 if (localStorage.getItem('theme') === 'light') {
-    body.classList.add('light-mode');
-    themeSwitch.checked = true;
+  body.classList.add('light-mode');
+  themeSwitch.checked = true;
 }
 
 themeSwitch.addEventListener('change', () => {
-    if (themeSwitch.checked) {
-        body.classList.add('light-mode');
-        localStorage.setItem('theme', 'light');
-    } else {
-        body.classList.remove('light-mode');
-        localStorage.setItem('theme', 'dark');
-    }
+  if (themeSwitch.checked) {
+    body.classList.add('light-mode');
+    localStorage.setItem('theme', 'light');
+  } else {
+    body.classList.remove('light-mode');
+    localStorage.setItem('theme', 'dark');
+  }
 });
